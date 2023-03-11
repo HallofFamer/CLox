5c6678fde9942fe58e6a8525bf81f99252067c3c
HallofFamer <worldoffame@hotmail.com>
2023-03-11 08:34:16 -0500

    minor change to object.c

--- src/vm/object.c
+++ src/vm/object.c
@@ -46,7 +46,7 @@ ObjClass* newClass(VM* vm, ObjString* name) {
     klass->name = name;
     klass->superclass = NULL;
     klass->isNative = false;
-    klass->internalType = OBJ_INSTANCE;
+    klass->internalType = -1;
     initTable(&klass->methods);
     return klass;
 }
