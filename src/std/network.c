84d02eb53b9aae9fa69583acc0d5007bb46ace70
HallofFamer <worldoffame@hotmail.com>
2023-08-18 16:53:57 -0400

    Add methods for URL class and tests for network package.

--- src/std/network.c
+++ src/std/network.c
@@ -9,14 +9,14 @@
 #include "../vm/value.h"
 #include "../vm/vm.h"
 
-LOX_METHOD(URI, init) {
-    ASSERT_ARG_COUNT("URI::init(scheme, host, port, path, query, fragment)", 6);
-    ASSERT_ARG_TYPE("URI::init(scheme, host, port, path, query, fragment)", 0, String);
-    ASSERT_ARG_TYPE("URI::init(scheme, host, port, path, query, fragment)", 1, String);
-    ASSERT_ARG_TYPE("URI::init(scheme, host, port, path, query, fragment)", 2, Int);
-    ASSERT_ARG_TYPE("URI::init(scheme, host, port, path, query, fragment)", 3, String);
-    ASSERT_ARG_TYPE("URI::init(scheme, host, port, path, query, fragment)", 4, String);
-    ASSERT_ARG_TYPE("URI::init(scheme, host, port, path, query, fragment)", 5, String);
+LOX_METHOD(URL, init) {
+    ASSERT_ARG_COUNT("URL::init(scheme, host, port, path, query, fragment)", 6);
+    ASSERT_ARG_TYPE("URL::init(scheme, host, port, path, query, fragment)", 0, String);
+    ASSERT_ARG_TYPE("URL::init(scheme, host, port, path, query, fragment)", 1, String);
+    ASSERT_ARG_TYPE("URL::init(scheme, host, port, path, query, fragment)", 2, Int);
+    ASSERT_ARG_TYPE("URL::init(scheme, host, port, path, query, fragment)", 3, String);
+    ASSERT_ARG_TYPE("URL::init(scheme, host, port, path, query, fragment)", 4, String);
+    ASSERT_ARG_TYPE("URL::init(scheme, host, port, path, query, fragment)", 5, String);
 
     ObjInstance* self = AS_INSTANCE(receiver);
     setObjProperty(vm, self, "scheme", args[0]);
@@ -28,8 +28,22 @@ LOX_METHOD(URI, init) {
     RETURN_OBJ(receiver);
 }
 
-LOX_METHOD(URI, toString) {
-    ASSERT_ARG_COUNT("URI::toString()", 0);
+LOX_METHOD(URL, isAbsolute) {
+    ASSERT_ARG_COUNT("URL::isAbsolute()", 0);
+    ObjInstance* self = AS_INSTANCE(receiver);
+    ObjString* host = AS_STRING(getObjProperty(vm, self, "host"));
+    RETURN_BOOL(host->length > 0);
+}
+
+LOX_METHOD(URL, isRelative) {
+    ASSERT_ARG_COUNT("URL::isRelative()", 0);
+    ObjInstance* self = AS_INSTANCE(receiver);
+    ObjString* host = AS_STRING(getObjProperty(vm, self, "host"));
+    RETURN_BOOL(host->length == 0);
+}
+
+LOX_METHOD(URL, toString) {
+    ASSERT_ARG_COUNT("URL::toString()", 0);
     ObjInstance* self = AS_INSTANCE(receiver);
     ObjString* scheme = AS_STRING(getObjProperty(vm, self, "scheme"));
     ObjString* host = AS_STRING(getObjProperty(vm, self, "host"));
@@ -38,8 +52,11 @@ LOX_METHOD(URI, toString) {
     ObjString* query = AS_STRING(getObjProperty(vm, self, "query"));
     ObjString* fragment = AS_STRING(getObjProperty(vm, self, "fragment"));
 
-    ObjString* uriString = formattedString(vm, "%s://%s", scheme->chars, host->chars);
-    if (port > 0 && port < 65536) uriString = formattedString(vm, "%s:%d", uriString->chars, port);
+    ObjString* uriString = newString(vm, "");
+    if (host->length > 0) {
+        uriString = (scheme->length > 0) ? formattedString(vm, "%s://%s", scheme->chars, host->chars) : host;
+        if (port > 0 && port < 65536) uriString = formattedString(vm, "%s:%d", uriString->chars, port);
+    }
     if (path->length > 0) uriString = formattedString(vm, "%s/%s", uriString->chars, path->chars);
     if (query->length > 0) uriString = formattedString(vm, "%s&%s", uriString->chars, query->chars);
     if (fragment->length > 0) uriString = formattedString(vm, "%s#%s", uriString->chars, fragment->chars);
@@ -50,10 +67,12 @@ void registerNetworkPackage(VM* vm) {
     ObjNamespace* networkNamespace = defineNativeNamespace(vm, "network", vm->stdNamespace);
     vm->currentNamespace = networkNamespace;
 
-    ObjClass* uriClass = defineNativeClass(vm, "URI");
-    bindSuperclass(vm, uriClass, vm->objectClass);
-    DEF_METHOD(uriClass, URI, init, 6);
-    DEF_METHOD(uriClass, URI, toString, 0);
+    ObjClass* urlClass = defineNativeClass(vm, "URL");
+    bindSuperclass(vm, urlClass, vm->objectClass);
+    DEF_METHOD(urlClass, URL, init, 6);
+    DEF_METHOD(urlClass, URL, isAbsolute, 0);
+    DEF_METHOD(urlClass, URL, isRelative, 0);
+    DEF_METHOD(urlClass, URL, toString, 0);
 
     vm->currentNamespace = vm->rootNamespace;
 }
