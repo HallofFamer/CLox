d4690f118e6433e77d4ec2eb3020d3feeeb6996d
HallofFamer <worldoffame@hotmail.com>
2023-08-20 12:00:09 -0400

    Add url parsing library yuarel.c

--- src/std/network.c
+++ src/std/network.c
@@ -2,7 +2,9 @@
 #include <string.h>
 
 #include "network.h"
+#include "../inc/yuarel.h"
 #include "../vm/assert.h"
+#include "../vm/dict.h"
 #include "../vm/native.h"
 #include "../vm/object.h"
 #include "../vm/string.h"
@@ -42,6 +44,55 @@ LOX_METHOD(URL, isRelative) {
     RETURN_BOOL(host->length == 0);
 }
 
+LOX_METHOD(URL, pathArray) {
+    ASSERT_ARG_COUNT("URL::pathArray()", 0);
+    ObjInstance* self = AS_INSTANCE(receiver);
+    ObjString* path = AS_STRING(getObjProperty(vm, self, "path"));
+    if (path->length == 0) RETURN_NIL;
+    else {
+        char* paths[UINT4_MAX];
+        int length = yuarel_split_path(path->chars, paths, 3);
+        if (length == -1) {
+            raiseError(vm, "Failed to parse path from URL.");
+            RETURN_NIL;
+        }
+
+        ObjArray* pathArray = newArray(vm);
+        push(vm, OBJ_VAL(pathArray));
+        for (int i = 0; i < length; i++) {
+            ObjString* subPath = newString(vm, paths[i]);
+            valueArrayWrite(vm, &pathArray->elements, OBJ_VAL(subPath));
+        }
+        pop(vm);
+        RETURN_OBJ(pathArray);
+    }
+}
+
+LOX_METHOD(URL, queryDict) {
+    ASSERT_ARG_COUNT("URL::queryDict()", 0);
+    ObjInstance* self = AS_INSTANCE(receiver);
+    ObjString* query = AS_STRING(getObjProperty(vm, self, "query"));
+    if (query->length == 0) RETURN_NIL;
+    else {
+        struct yuarel_param params[UINT4_MAX];
+        int length = yuarel_parse_query(query->chars, '&', params, UINT4_MAX);
+        if (length == -1) {
+            raiseError(vm, "Failed to parse query parameters from URL.");
+            RETURN_NIL;
+        }
+
+        ObjDictionary* queryDict = newDictionary(vm);
+        push(vm, OBJ_VAL(queryDict));
+        for (int i = 0; i < length; i++) {
+            ObjString* key = newString(vm, params[i].key);
+            ObjString* value = newString(vm, params[i].val);
+            dictSet(vm, queryDict, OBJ_VAL(key), OBJ_VAL(value));
+        }     
+        pop(vm);
+        RETURN_OBJ(queryDict);
+    }
+}
+
 LOX_METHOD(URL, toString) {
     ASSERT_ARG_COUNT("URL::toString()", 0);
     ObjInstance* self = AS_INSTANCE(receiver);
@@ -63,6 +114,26 @@ LOX_METHOD(URL, toString) {
     RETURN_OBJ(uriString);
 }
 
+LOX_METHOD(URLClass, parse) {
+    ASSERT_ARG_COUNT("URL class::parse(url)", 1);
+    ASSERT_ARG_TYPE("URL class::parse(url)", 0, String);
+    ObjInstance* instance = newInstance(vm, AS_CLASS(receiver));
+    ObjString* url = AS_STRING(args[0]);
+    struct yuarel component;
+    if (yuarel_parse(&component, url->chars) == -1) {
+        raiseError(vm, "Failed to parse url.");
+        RETURN_NIL;
+    }
+
+    setObjProperty(vm, instance, "scheme", OBJ_VAL(newString(vm, component.scheme != NULL ? component.scheme : "")));
+    setObjProperty(vm, instance, "host", OBJ_VAL(newString(vm, component.host != NULL ? component.host : "")));
+    setObjProperty(vm, instance, "port", INT_VAL(component.port));
+    setObjProperty(vm, instance, "path", OBJ_VAL(newString(vm, component.path != NULL ? component.path : "")));
+    setObjProperty(vm, instance, "query", OBJ_VAL(newString(vm, component.query != NULL ? component.query : "")));
+    setObjProperty(vm, instance, "fragment", OBJ_VAL(newString(vm, component.fragment != NULL ? component.fragment : "")));
+    RETURN_OBJ(instance);
+}
+
 void registerNetworkPackage(VM* vm) {
     ObjNamespace* networkNamespace = defineNativeNamespace(vm, "network", vm->stdNamespace);
     vm->currentNamespace = networkNamespace;
@@ -72,7 +143,12 @@ void registerNetworkPackage(VM* vm) {
     DEF_METHOD(urlClass, URL, init, 6);
     DEF_METHOD(urlClass, URL, isAbsolute, 0);
     DEF_METHOD(urlClass, URL, isRelative, 0);
+    DEF_METHOD(urlClass, URL, pathArray, 0);
+    DEF_METHOD(urlClass, URL, queryDict, 0);
     DEF_METHOD(urlClass, URL, toString, 0);
+
+    ObjClass* urlMetaclass = urlClass->obj.klass;
+    DEF_METHOD(urlMetaclass, URLClass, parse, 1);
 
     vm->currentNamespace = vm->rootNamespace;
 }
