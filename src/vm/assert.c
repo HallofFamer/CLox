dd552f0e61204a5904e01484ce6a43d0deefa7b8
HallofFamer <worldoffame@hotmail.com>
2023-11-10 08:48:55 -0500

    Update assert.h and assert.c to throw exceptions on assertion failure.

--- src/vm/assert.c
+++ src/vm/assert.c
@@ -5,179 +5,147 @@
 #include "native.h"
 #include "vm.h"
 
-void assertArgCount(VM* vm, const char* method, int expectedCount, int actualCount) {
+Value assertArgCount(VM* vm, const char* method, int expectedCount, int actualCount) {
     if (expectedCount != actualCount) {
-        runtimeError(vm, "method %s expects %d argument(s) but got %d instead.", method, expectedCount, actualCount);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects %d argument(s) but got %d instead.", method, expectedCount, actualCount);
     }
+    RETURN_NIL;
 }
 
-void assertArgInstanceOf(VM* vm, const char* method, Value* args, int index, const char* className) {
+Value assertArgInstanceOf(VM* vm, const char* method, Value* args, int index, const char* className) {
     if (!isObjInstanceOf(vm, args[index], getNativeClass(vm, className))) {
-        runtimeError(vm, "method %s expects argument %d to be an instance of class %s but got %s.", 
+        RETURN_STRING_FMT("method %s expects argument %d to be an instance of class %s but got %s.",
             method, index + 1, className, getObjClass(vm, args[index])->name->chars);
-        exit(70);
     }
+    RETURN_NIL;
 }
 
-void assertArgInstanceOfAny(VM* vm, const char* method, Value* args, int index, const char* className, const char* className2) {
+Value assertArgInstanceOfAny(VM* vm, const char* method, Value* args, int index, const char* className, const char* className2) {
     if (!isObjInstanceOf(vm, args[index], getNativeClass(vm, className)) && !isObjInstanceOf(vm, args[index], getNativeClass(vm, className2))) {
-        runtimeError(vm, "method %s expects argument %d to be an instance of class %s or %s but got %s.",
+        RETURN_STRING_FMT("method %s expects argument %d to be an instance of class %s or %s but got %s.",
             method, index + 1, className, className2, getObjClass(vm, args[index])->name->chars);
-        exit(70);
     }   
+    RETURN_NIL;
 }
 
-void assertArgIsArray(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsArray(VM* vm, const char* method, Value* args, int index) {
     if (!IS_ARRAY(args[index]) && !isObjInstanceOf(vm, args[index], vm->arrayClass)) {
-        runtimeError(vm, "method %s expects argument %d to be an array.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be an array.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsBool(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsBool(VM* vm, const char* method, Value* args, int index) {
     if (!IS_BOOL(args[index]) && !isObjInstanceOf(vm, args[index], vm->boolClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a boolean value.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a boolean value.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsClass(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsClass(VM* vm, const char* method, Value* args, int index) {
     if (!IS_CLASS(args[index]) && !isObjInstanceOf(vm, args[index], vm->classClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a class.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a class.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsClosure(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsClosure(VM* vm, const char* method, Value* args, int index) {
     if (!IS_CLOSURE(args[index]) && !isObjInstanceOf(vm, args[index], vm->functionClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a closure.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a closure.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsDictionary(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsDictionary(VM* vm, const char* method, Value* args, int index) {
     if (!IS_DICTIONARY(args[index]) && !isObjInstanceOf(vm, args[index], vm->dictionaryClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a dictionary.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a dictionary.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsEntry(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsEntry(VM* vm, const char* method, Value* args, int index) {
     if (!IS_ENTRY(args[index]) && !isObjInstanceOf(vm, args[index], vm->entryClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a map entry.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a map entry.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsException(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsException(VM* vm, const char* method, Value* args, int index) {
     if (!IS_INSTANCE(args[index]) && !isObjInstanceOf(vm, args[index], vm->exceptionClass)) {
-        runtimeError(vm, "method %s expects argument %d to be an exception.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be an exception.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsFile(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsFile(VM* vm, const char* method, Value* args, int index) {
     if (!IS_FILE(args[index]) && !isObjInstanceOf(vm, args[index], vm->fileClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a file.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a file.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsFloat(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsFloat(VM* vm, const char* method, Value* args, int index) {
     if (!IS_FLOAT(args[index]) && !isObjInstanceOf(vm, args[index], vm->floatClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a floating point number.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a floating point number.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsInt(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsInt(VM* vm, const char* method, Value* args, int index) {
     if (!IS_INT(args[index]) && !isObjInstanceOf(vm, args[index], vm->intClass)) {
-        runtimeError(vm, "method %s expects argument %d to be an integer number.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be an integer number.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsMethod(VM* vm, const char* method, Value* args, int index) { 
+Value assertArgIsMethod(VM* vm, const char* method, Value* args, int index) { 
     if (!IS_NAMESPACE(args[index]) && !isObjInstanceOf(vm, args[index], vm->methodClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a method.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a method.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsNamespace(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsNamespace(VM* vm, const char* method, Value* args, int index) {
     if (!IS_NAMESPACE(args[index]) && !isObjInstanceOf(vm, args[index], vm->namespaceClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a namespace.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a namespace.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsNode(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsNode(VM* vm, const char* method, Value* args, int index) {
     if (!IS_NODE(args[index]) && !isObjInstanceOf(vm, args[index], vm->nodeClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a link node.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a link node.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsNumber(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsNumber(VM* vm, const char* method, Value* args, int index) {
     if (!IS_NUMBER(args[index]) && !isObjInstanceOf(vm, args[index], vm->numberClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a number.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a number.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsRange(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsRange(VM* vm, const char* method, Value* args, int index) {
     if (!IS_RANGE(args[index]) && !isObjInstanceOf(vm, args[index], vm->rangeClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a range.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a range.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertArgIsString(VM* vm, const char* method, Value* args, int index) {
+Value assertArgIsString(VM* vm, const char* method, Value* args, int index) {
     if (!IS_STRING(args[index]) && !isObjInstanceOf(vm, args[index], vm->stringClass)) {
-        runtimeError(vm, "method %s expects argument %d to be a string.", method, index + 1);
-        exit(70);
+        RETURN_STRING_FMT("method %s expects argument %d to be a string.", method, index + 1);
     }
+    RETURN_NIL;
 }
 
-void assertIntWithinRange(VM* vm, const char* method, int value, int min, int max, int index){
+Value assertIndexWithinBounds(VM* vm, const char* method, int value, int min, int max, int index){
     if (value < min || value > max) {
-        runtimeError(vm, "method %s expects argument %d to be an integer within range %d to %d but got %d.", 
+        RETURN_STRING_FMT("method %s expects argument %d to be an integer within range %d to %d but got %d.",
             method, index + 1, min, max, value);
-        exit(70);
     }
-}
-
-void assertNumberNonNegative(VM* vm, const char* method, double number, int index) {
-    if (number < 0) {
-        if (index < 0) runtimeError(vm, "method %s expects receiver to be a non negative number but got %g.", method, number);
-        else runtimeError(vm, "method %s expects argument %d to be a non negative number but got %g.", method, index + 1, number);
-        exit(70);
-    }
-}
-
-void assertNumberNonZero(VM* vm, const char* method, double number, int index) {
-    if (number == 0) {
-        if (index < 0) runtimeError(vm, "method %s expects receiver to be a non-zero number but got %g.", method, number);
-        else runtimeError(vm, "method %s expects argument %d to be a non-zero number but got %g.", method, index + 1, number);
-        exit(70);
-    }
-}
-
-void assertNumberPositive(VM* vm, const char* method, double number, int index) {
-    if (number <= 0) {
-        if (index < 0) runtimeError(vm, "method %s expects receiver to be a positive number but got %g.", method, number);
-        else runtimeError(vm, "method %s expects argument %d to be a positive number but got %g.", method, index + 1, number);
-        exit(70);
-    }
-}
-
-void assertNumberWithinRange(VM* vm, const char* method, double value, double min, double max, int index) {
-    if (value < min || value > max) {
-        runtimeError(vm, "method %s expects argument %d to be a number within range %g to %g but got %g.", 
-            method, index + 1, min, max, value);
-        exit(70);
-    }
+    RETURN_NIL;
 }
 
 void raiseError(VM* vm, const char* message) {
--- src/vm/assert.h
+++ src/vm/assert.h
@@ -3,37 +3,59 @@
 #define clox_assert_h
 
 #include "common.h"
+#include "native.h"
 #include "value.h"
 
-#define ASSERT_ARG_COUNT(method, expectedCount) assertArgCount(vm, method, expectedCount, argCount)
-#define ASSERT_ARG_INSTANCE_OF(method, index, className) assertArgInstanceOf(vm, method, args, index, #className)
-#define ASSERT_ARG_INSTANCE_OF_ANY(method, index, className, className2) assertArgInstanceOfAny(vm, method, args, index, #className, #className2)
-#define ASSERT_ARG_TYPE(method, index, type) assertArgIs##type(vm, method, args, index)
+#define ASSERT_ARG_COUNT(method, expectedCount) \
+    do { \
+       Value message = assertArgCount(vm, method, expectedCount, argCount); \
+       if (IS_STRING(message)) THROW_EXCEPTION(clox.std.lang.IllegalArgumentException, AS_CSTRING(message)); \
+    } while (false)
 
-void assertArgCount(VM* vm, const char* method, int expectedCount, int actualCount);
-void assertArgInstanceOf(VM* vm, const char* method, Value* args, int index, const char* className);
-void assertArgInstanceOfAny(VM* vm, const char* method, Value* args, int index, const char* className, const char* className2);
-void assertArgIsArray(VM* vm, const char* method, Value* args, int index);
-void assertArgIsBool(VM* vm, const char* method, Value* args, int index);
-void assertArgIsClass(VM* vm, const char* method, Value* args, int index);
-void assertArgIsClosure(VM* vm, const char* method, Value* args, int index);
-void assertArgIsDictionary(VM* vm, const char* method, Value* args, int index);
-void assertArgIsEntry(VM* vm, const char* method, Value* args, int index);
-void assertArgIsException(VM* vm, const char* method, Value* args, int index);
-void assertArgIsFile(VM* vm, const char* method, Value* args, int index);
-void assertArgIsFloat(VM* vm, const char* method, Value* args, int index);
-void assertArgIsInt(VM* vm, const char* method, Value* args, int index);
-void assertArgIsMethod(VM* vm, const char* method, Value* args, int index);
-void assertArgIsNamespace(VM* vm, const char* method, Value* args, int index);
-void assertArgIsNode(VM* vm, const char* method, Value* args, int index);
-void assertArgIsNumber(VM* vm, const char* method, Value* args, int index);
-void assertArgIsRange(VM* vm, const char* method, Value* args, int index);
-void assertArgIsString(VM* vm, const char* method, Value* args, int index);
-void assertIntWithinRange(VM* vm, const char* method, int value, int min, int max, int index);
-void assertNumberNonNegative(VM* vm, const char* method, double number, int index);
-void assertNumberNonZero(VM* vm, const char* method, double number, int index);
-void assertNumberPositive(VM* vm, const char* method, double number, int index);
-void assertNumberWithinRange(VM* vm, const char* method, double value, double min, double max, int index);
+#define ASSERT_ARG_INSTANCE_OF(method, index, className) \
+    do { \
+        Value message = assertArgInstanceOf(vm, method, args, index, #className); \
+        if (IS_STRING(message)) THROW_EXCEPTION(clox.std.lang.IllegalArgumentException, AS_CSTRING(message)); \
+    } while (false)
+
+#define ASSERT_ARG_INSTANCE_OF_ANY(method, index, className, className2) \
+    do { \
+        Value message = assertArgInstanceOfAny(vm, method, args, index, #className, #className2); \
+        if (IS_STRING(message)) THROW_EXCEPTION(clox.std.lang.IllegalArgumentException, AS_CSTRING(message)); \
+    } while (false)
+
+#define ASSERT_ARG_TYPE(method, index, type) \
+    do { \
+        Value message = assertArgIs##type(vm, method, args, index); \
+        if (IS_STRING(message)) THROW_EXCEPTION(clox.std.lang.IllegalArgumentException, AS_CSTRING(message)); \
+    } while (false)
+
+#define ASSERT_INDEX_WITHIN_BOUNDS(method, value, min, max, index) \
+    do { \
+        Value message = assertIndexWithinBounds(vm, method, value, min, max, index); \
+        if (IS_STRING(message)) THROW_EXCEPTION(clox.std.lang.IndexOutOfBoundsException, AS_CSTRING(message)); \
+    } while (false)
+
+Value assertArgCount(VM* vm, const char* method, int expectedCount, int actualCount);
+Value assertArgInstanceOf(VM* vm, const char* method, Value* args, int index, const char* className);
+Value assertArgInstanceOfAny(VM* vm, const char* method, Value* args, int index, const char* className, const char* className2);
+Value assertArgIsArray(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsBool(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsClass(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsClosure(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsDictionary(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsEntry(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsException(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsFile(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsFloat(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsInt(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsMethod(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsNamespace(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsNode(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsNumber(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsRange(VM* vm, const char* method, Value* args, int index);
+Value assertArgIsString(VM* vm, const char* method, Value* args, int index);
+Value assertIndexWithinBounds(VM* vm, const char* method, int value, int min, int max, int index);
 void raiseError(VM* vm, const char* message);
 
 #endif // !clox_assert_h
