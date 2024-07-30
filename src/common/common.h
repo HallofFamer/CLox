098f289ccf26b03fd19cd939122cec235df2ac05
HallofFamer <worldoffame@hotmail.com>
2024-07-30 08:50:19 -0400

    Start of big refactoring for CLox 2.0.0 dev, add new src subdirectory 'common' and 'compiler', and source files common.h, lexer.h and lexer.c.

---
+++ src/common/common.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef clox_common_h
+#define clox_common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define NAN_BOXING
+//#define DEBUG_PRINT_CODE
+//#define DEBUG_TRACE_EXECUTION
+
+//#define DEBUG_PRINT_SHAPE
+//#define DEBUG_TRACE_CACHE
+
+//#define DEBUG_STRESS_GC
+//#define DEBUG_LOG_GC
+
+#define UINT8_COUNT (UINT8_MAX + 1)
+#define UINT4_MAX 15
+#define UINT4_COUNT (UINT4_MAX + 1)
+#define MAX_CASES 256
+
+typedef struct VM VM;
+typedef enum InterpretResult InterpretResult;
+typedef struct CallFrame CallFrame;
+typedef struct Compiler Compiler;
+typedef struct ClassCompiler ClassCompiler;
+
+#endif // !clox_common_h
